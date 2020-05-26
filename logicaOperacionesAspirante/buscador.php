1c39b633413048a3b4110410e90afcc3e5234b6a
Luis Enrique <luisechvz@gmail.com>
2020-05-26 02:11:53 -0500

    boton de busqueda

--- logicaOperacionesAspirante/buscador.php
+++ logicaOperacionesAspirante/buscador.php
@@ -1,21 +1,32 @@
 <?php
-
-$busca = $_POST["busca"];
-
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "u253306330_curvity";
+$password = "curvity";
+$dbname = "u253306330_curvity";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+
+if (isset($_POST['submit'])) {
+    $busca = $_POST["busca"];
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    if($busca != ""){
+        $sql = "SELECT Nombre FROM Empresa WHERE Nombre like '%".$busca."%'";
+        $result = $conn->query($sql);
+    }
+
+    if ($result = $conn->query($sql)) {
+
+        /* fetch associative array */
+        while ($row = $result->fetch_assoc()) {
+            printf ("%s (%s)\n", $row["Nombre"]);
+        }
+    
+        /* free result set */
+        $result->close();
+    }
+
 }
-
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = $conn->query($sql);
-
-
-?>

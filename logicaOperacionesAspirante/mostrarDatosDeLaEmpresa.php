46a0b7db88e66869701f774a85d45f0382aca957
Luis Enrique <luisechvz@gmail.com>
2020-05-26 16:25:08 -0500

    Agregue un fichero JSON para vaciar los datos de la empresa

--- logicaOperacionesAspirante/mostrarDatosDeLaEmpresa.php
+++ logicaOperacionesAspirante/mostrarDatosDeLaEmpresa.php
@@ -1,24 +1,51 @@
 <?php
-$servername = "localhost";
-$username = "u253306330_curvity";
-$password = "curvity";
-$dbname = "u253306330_curvity";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-
-$salida = "";
-
-$query = "SELECT * FROM Empresa ORDER By IDEmpresa";
-
-if ($resultado = $conn->query($query)) {
-    while ($fila = $resultado->fetch_assoc()) {
-
-        printf ("%s (%s)\n", $fila["Nombre"], $fila["IDEmpresa"]);
+function conectar(){
+    $servername = "localhost";
+    $username = "u253306330_curvity";
+    $password = "curvity";
+    $dbname = "u253306330_curvity";
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+        exit();
     }
-    $resultado->free();
+    return $conn;
 }
 
-$conn->close();
+//Indicamos el tipo de archivo que se generara
+header('Content-Type: application/json');
+session_start();
+
+//Nos conectamos a la base de datos
+$c=conectar();
+mysqli_set_charset($c,"utf8");
+
+// Indicamos nuestra query
+$consulta=$c->query("SELECT * FROM Empresa");
+ 
+
+//En caso de error en la consulta nos muestra el perro error
+if(!$consulta){
+    die("Error en la consulta SQL");
+}
+
+//Metemos todo en un array
+while($sql=$consulta->fetch_assoc()){
+    $sqlArray["data"][]=$sql;    
+}
+
+$mostrarJSON=false; // true: mostrará los datos JSON en php | false: guardará el fichero json en un fichero .json 
+if($mostrarJSON){
+    echo json_encode($sqlArray);
+}else{
+    $listaTiendas = "../json/listaEmpresa.json";
+    $data = json_encode($sqlArray); 
+    
+    // Ojo, deberemos de tener creada la carpeta 'json'
+    if ($fp = fopen($listaEmpleos, "w")){
+    fwrite($fp, $data);
+    }
+    fclose($fp);
+}

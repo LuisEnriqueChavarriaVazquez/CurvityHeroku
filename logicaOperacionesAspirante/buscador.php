7a921cce1bf7d1589986731d5c40f10f26aad6ba
Luis Enrique <luisechvz@gmail.com>
2020-05-26 04:43:55 -0500

    corregi mensaje raro en las busquedas

--- logicaOperacionesAspirante/buscador.php
+++ logicaOperacionesAspirante/buscador.php
@@ -1,54 +1,55 @@
 <?php
-	$servername = "localhost";
-    $username = "u253306330_curvity";
-  	$password = "curvity";
-  	$dbname = "u253306330_curvity";
+$servername = "localhost";
+$username = "u253306330_curvity";
+$password = "curvity";
+$dbname = "u253306330_curvity";
 
-	$conn = new mysqli($servername, $username, $password, $dbname);
-      if($conn->connect_error){
-        die("Conexión fallida: ".$conn->connect_error);
-      }
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
 
-    $salida = "";
+$salida = "";
 
-    $query = "SELECT * FROM Empresa WHERE Name NOT LIKE '' ORDER By IDEmpresa LIMIT 5";
+$query = "SELECT * FROM Empresa WHERE Name NOT LIKE '' ORDER By IDEmpresa LIMIT 5";
 
-    if (isset($_POST['consulta'])) {
-    	$q = $conn->real_escape_string($_POST['consulta']);
-    	$query = "SELECT * FROM Empresa WHERE Nombre LIKE '%$q%'";
-    }
+if (isset($_POST['consulta'])) {
+    $q = $conn->real_escape_string($_POST['consulta']);
+    $query = "SELECT * FROM Empresa WHERE Nombre LIKE '%$q%'";
+}
 
-    $resultado = $conn->query($query);
+$resultado = $conn->query($query);
 
-    if ($resultado->num_rows>0) {
-        $salida.="<div>
+if ($result = $conn->query($sql)) {
+    if ($resultado->num_rows > 0) {
+        $salida .= "<div>
         
         
 
 
         ";
 
-    	while ($fila = $resultado->fetch_assoc()) {
-    		$salida.="
+        while ($fila = $resultado->fetch_assoc()) {
+            $salida .= "
             <div class='col s6 m6 borderCardInicio'>
       <a href='operacionesAspirante.php'>
         <div class='card borderCardInicio waves-effect'>
-        <div class='card-image'>"."<img class='card-image' src='data:image/jpeg; base64,".base64_encode($fila['FotoLogo'])."'>"."</div>
+        <div class='card-image'>" . "<img class='card-image' src='data:image/jpeg; base64," . base64_encode($fila['FotoLogo']) . "'>" . "</div>
           <div class='card-content'>
-            <p class='flow-text noLinkStyle'>".$fila['Nombre']."</p>
+            <p class='flow-text noLinkStyle'>" . $fila['Nombre'] . "</p>
           </div>
         </div>
       </a>
     </div>
     				";
-
-    	}
-    	$salida.="</div>";
-    }else{
-    	$salida.="NO HAY DATOS :(";
+        }
+        $salida .= "</div>";
+    } else {
+        $salida .= "NO HAY DATOS :(";
     }
+}
 
 
-    echo $salida;
+echo $salida;
 
-    $conn->close();
+$conn->close();

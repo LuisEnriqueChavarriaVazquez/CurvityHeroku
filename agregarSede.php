ca240c58d7304e91780ff73acb021d4d6092241e
Luis Enrique <luisechvz@gmail.com>
2020-05-25 19:08:18 -0500

    validacion del formulario para nuevas sedes

--- agregarSede.php
+++ agregarSede.php
@@ -168,7 +168,7 @@ gestionSedes.php
             </div>
         </form>
     </div>
-    <a href="gestionSedes.php"><button type="submit" class="waves-effect btn-large borderButton sizeButton textButton grey lighten-5 blue-text text-darken-4">Agregar sede.</button></a>
+    <a><button type="submit" class="waves-effect btn-large borderButton sizeButton textButton grey lighten-5 blue-text text-darken-4">Agregar sede.</button></a>
 </div>
 
 <?php include 'AlmacenIncludesPHP/elementosPhp/HTMLSTRUCTURE/parteInferior.php' ?>

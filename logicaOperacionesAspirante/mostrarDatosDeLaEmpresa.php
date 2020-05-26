73a0af2ff1e040498dbb288ecd8ffa08c1d39e36
Luis Enrique <luisechvz@gmail.com>
2020-05-26 10:08:27 -0500

    Prueba con arrays asociativo2

--- logicaOperacionesAspirante/mostrarDatosDeLaEmpresa.php
+++ logicaOperacionesAspirante/mostrarDatosDeLaEmpresa.php
@@ -13,12 +13,6 @@ $salida = "";
 
 $query = "SELECT * FROM Empresa ORDER By IDEmpresa";
 
-if($resultado = $conn->query($query)){
-    while($fila = $resultado->fetch_row()){
-        printf("%s (%s)\n", $fila[0], $fila[1]);
-    }
-}
-
 if ($resultado = $conn->query($query)) {
     while ($fila = $resultado->fetch_assoc()) {
         printf ("%s (%s)\n", $fila["Nombre"], $fila["IDEmpresa"]);
@@ -26,4 +20,4 @@ if ($resultado = $conn->query($query)) {
     $resultado->free();
 }
 
-$resultado->close();
+$conn->close();

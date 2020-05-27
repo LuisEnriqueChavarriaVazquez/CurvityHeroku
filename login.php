3d584818380707ee54953fa18c6454c6055b4cbe
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2020-05-27 11:42:24 -0500

    Correcion logIn principal

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ index.php
 <?php include 'AlmacenIncludesPHP/elementosPhp/navbarRetorno/navbarInicialRetornoInferior.php' ?>
 
 <div class="boxSubjectsInicioBigger light-blue darken-4 centerElements">
-    <?php include 'AlmacenIncludesPHP/elementosPhp/cards/cardLogIn.php' ?>
+   <?php //include 'AlmacenIncludesPHP/elementosPhp/cards/cardLogIn.php' ?>
 
     <a href="index.php" class="waves-effect btn-large borderButton sizeButton textButton grey lighten-5 blue-text text-darken-4">Volver</a>
 </div>

8f6a4ebda55bc11e88c964846e77f08e121ee125
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2020-05-27 12:15:00 -0500

    Correcion logIn principal

--- AlmacenIncludesPHP/elementosPhp/cards/cardLogIn.php
+++ AlmacenIncludesPHP/elementosPhp/cards/cardLogIn.php
@@ -1,7 +1,7 @@
 <div class="sizeCardInicio backgroundCardInicio centerElements borderCardInicio z-depth-3">
     <img src="././pictures/aspirante.png" class="imagesSize">
     <a href="index_asp.php" class="waves-effect waves-light btn-large borderButton sizeButton transparent textButton buttonDesignJustBorder">Log in aspirante</a>
-   <!-- <img src="././pictures/empresa.png" class="imagesSize">
+    <img src="././pictures/empresa.png" class="imagesSize">
     <a href="index_emp.php" class="waves-effect waves-light btn-large borderButton sizeButton transparent textButton buttonDesignJustBorder">Log in empresa</a>
  <img src="././pictures/reclutador.png" class="imagesSize wither">
     <a href="loginReclutador.php" class="waves-effect waves-light btn-large borderButton sizeButton transparent textButton buttonDesignJustBorder">Log in reclutador</a>-->

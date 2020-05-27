19d3d555a5c6717864be90e4b5964168e5671ef2
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2020-05-27 18:07:05 -0500

    ingreso reclutador

--- validacionLogInReclutador.php
+++ validacionLogInReclutador.php
@@ -3,7 +3,7 @@ require('claseDB.php');
 
 if (isset($_POST['submit'])) {
      $correoEntrada=$_POST["username_emp"];
-     
+     echo "<div class=´errors_box´><p class='errors'>" . "Correo no  coincide" . "</p></div>";
      $objetoConexion=new objetoConexionBaseDatos();
      if($objetoConexion-> comprobarConexion()==TRUE){
          if($objetoConexion-> comprobarExistenciaElementoAtibuto("Empresa","CorreoElecReclutador",$correoEntrada)==TRUE){

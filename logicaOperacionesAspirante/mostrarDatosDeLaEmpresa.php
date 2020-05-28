b1430047f1071eb244525171ee7204bb455af7fb
Luis Enrique <luisechvz@gmail.com>
2020-05-28 06:30:38 -0500

    Primera version del nuevo perfil

--- logicaOperacionesAspirante/mostrarDatosDeLaEmpresa.php
+++ logicaOperacionesAspirante/mostrarDatosDeLaEmpresa.php
@@ -1,17 +1,1 @@
-<?php
-/*$servername = "localhost";
-$username = "u253306330_curvity";
-$password = "curvity";
-$dbname = "u253306330_curvity";
 
-$conn = mysqli_connect('localhost', 'u253306330_curvity', 'curvity', 'u253306330_curvity');
-
-$selct = mysqli_query($conn,"select * from Empresa");
-while ($dat = mysqli_fetch_assoc($selct)){
-    $arr[] = $dat;
-}
-
-echo json_encode($arr);*/
-if(isset($_POST['submit'])){
-    echo "Funciono";
-}

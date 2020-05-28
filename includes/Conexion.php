c89fe617f9e863de2942673afcda8341a127344a
ulisesJ01 <56226569+ulisesJ01@users.noreply.github.com>
2020-05-28 13:12:13 -0500

    Update Conexion.php

--- includes/Conexion.php
+++ includes/Conexion.php
@@ -2,9 +2,9 @@
 
 	class conectar{
 		private $servidor="localhost";
-		private $usuario="root";
-		private $password="";
-		private $bd="curvity";
+		private $usuario="u253306330_curvity";
+		private $password="curvity";
+		private $bd="u253306330_curvity";
 
 		public function conexion(){
 			$conexion=mysqli_connect($this->servidor,

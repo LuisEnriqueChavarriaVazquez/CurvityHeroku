b0373060d6b40fb8f43512ad1847806c9a64dbe2
ulisesJ01 <56226569+ulisesJ01@users.noreply.github.com>
2020-05-29 12:08:42 -0500

    prueba update aspirante

--- editarPerfilAspirante.php
+++ editarPerfilAspirante.php
@@ -3,9 +3,6 @@ include_once 'includes/user.php';
 include_once 'includes/user_session.php';
 include 'includes/Conexion.php';
 
-$userSession = new UserSession();
-$user = new User();
-
 if(isset($_SESSION['user'])){
     $user->setUser($userSession->getCurrentUser());
     $dato=$user->getCorreo();

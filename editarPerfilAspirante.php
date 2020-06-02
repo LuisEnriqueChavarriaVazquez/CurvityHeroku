1d433c7d4be567b7c3c7a9e660d8dae9f90d88a0
ulisesJ01 <56226569+ulisesJ01@users.noreply.github.com>
2020-06-02 14:17:50 -0500

    ver img

--- editarPerfilAspirante.php
+++ editarPerfilAspirante.php
@@ -273,9 +273,9 @@ index_asp.php
                 <div class="file-field input-field">
                     <div class="btn white blue-text text-darken-4">
                         <span>Foto de perfil</span>
-                        <img src="data:image/jpeg;base64,<?php base64_encode($datos[17])?>" />
                         <input type="file" id="archivo_aspirante" name="archivo_aspirante"/>
                     </div>
+                    <img src="data:image/jpeg;base64,<?php base64_encode($datos[17])?>" />
                     <div class="file-path-wrapper">
                         <input class="file-path validate" id="archivoDir_aspirante" name="archivoDir_aspirante" 
                         value="<?php
